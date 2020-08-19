6f4405f973719a2b8f41113b86c01fcc92a3d12e
Remyson Rodrigues <remysonrodrigues@gmail.com>
2020-08-19 10:44:05 -0300

    add route produtos/new

--- loja/app/controllers/produtos_controller.rb
+++ loja/app/controllers/produtos_controller.rb
@@ -1,8 +1,15 @@
 class ProdutosController < ApplicationController
 
   def index
-    @produtos = Produto.order(nome: :desc).limit 1
+    @produtos = Produto.order(nome: :desc).limit 4
     @produto_com_desconto = Produto.order(:preco).limit 1
   end
+
+  def create
+    produto = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade)
+    Produto.create produto
+    redirect_to root_path # Retorna para pagina principal
+  end
+  
   
 end
