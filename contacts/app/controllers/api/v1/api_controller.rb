970407df564468fc487f1ca3d58a610c017debd9
Remyson Rodrigues <remysonrodrigues@gmail.com>
2020-08-21 11:39:32 -0300

    add simple token

--- contacts/app/controllers/api/v1/api_controller.rb
+++ contacts/app/controllers/api/v1/api_controller.rb
@@ -1,5 +1,14 @@
 module Api::V1
   class ApiController < ApplicationController
     # Métodos globais
+    acts_as_token_authentication_handle_for User
+    before_action :require_authentication!
+
+    private
+
+    def require_authentication!
+      throw(:warden, scope: :user) unless current_user.presence
+    end
+    
   end
 end
--- contacts/db/schema.rb
+++ contacts/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_142240) do
+ActiveRecord::Schema.define(version: 2020_08_21_143118) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_08_21_142240) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "authentication_token", limit: 30
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
