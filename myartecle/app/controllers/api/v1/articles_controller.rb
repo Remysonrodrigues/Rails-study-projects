f45e5167114184f311eee75ceddff0a2f9799d99
Remyson Rodrigues <remysonrodrigues@gmail.com>
2020-08-22 11:19:09 -0300

    add route show

--- myartecle/app/controllers/api/v1/articles_controller.rb
+++ myartecle/app/controllers/api/v1/articles_controller.rb
@@ -3,8 +3,13 @@ module Api
     class ArticlesController < ApplicationController
       # Listar todos os artigos
       def index
-        articles = Article.order('creted_at DESC')
+        articles = Article.order('created_at DESC')
         render json: { status: 'SUCCESS', message: 'Artigos carregados', data: articles }, status: :ok
+      end
+      # Listar artigo passando ID
+      def show
+        article = Article.find(params[:id])
+        render json: { status: 'SUCCESS', message: 'Loaded article', data: article }, status: :ok
       end
       
     end
