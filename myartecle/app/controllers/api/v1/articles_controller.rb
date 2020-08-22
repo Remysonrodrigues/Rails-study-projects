a372df5bbddffacbc0812fc48179c09f426da94c
Remyson Rodrigues <remysonrodrigues@gmail.com>
2020-08-22 11:43:09 -0300

    add route update

--- myartecle/app/controllers/api/v1/articles_controller.rb
+++ myartecle/app/controllers/api/v1/articles_controller.rb
@@ -26,7 +26,15 @@ module Api
         article.destroy
         render json: { status: 'SUCCESS', message: 'Deleted article', data: article }, status: :ok
       end
-      
+      # Atualizar um artigo
+      def update
+        article = Article.find(params[:id])
+        if article.update_attributes(article_params)
+          render json: { status: 'SUCCESS', message: 'Updated article', data: article }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Articles not update', data: article.erros }, status: :unprocessable_entity
+        end
+      end
 
       private
         # Parametros aceitos
