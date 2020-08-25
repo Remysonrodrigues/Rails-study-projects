d087c4a7dd44d6b9a4026377ca7a7efa52744311
Remyson Rodrigues <remysonrodrigues@gmail.com>
2020-08-25 09:58:12 -0300

    add gem jwt

--- tasks-api/db/schema.rb
+++ tasks-api/db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2020_08_25_122824) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "nama"
+    t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
