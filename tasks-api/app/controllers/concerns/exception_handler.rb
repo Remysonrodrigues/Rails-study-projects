537aa3ff5b8255fa60b06d14c10d35700da932b7
Remyson Rodrigues <remysonrodrigues@gmail.com>
2020-08-25 10:16:01 -0300

    add exception_handler jwt

--- tasks-api/app/controllers/concerns/exception_handler.rb
+++ tasks-api/app/controllers/concerns/exception_handler.rb
@@ -2,13 +2,37 @@ module ExceptionHandler
   # fornece o método `included` mais elegante
   extend ActiveSupport::Concern
 
+  # Definir subclasses de erro personalizadas - resgate captura `StandardErrors`
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+
   included do
+
+    # Defina manipuladores personalizados
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
     end
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
+    # rescue_from ActiveRecord::RecordInvalid do |e|
+      # json_response({ message: e.message }, :unprocessable_entity)
+    # end
+  end
+
+  private
+    # Resposta JSON com mensagem; Código de status 422 - entidade não processável
+    def four_twenty_two(e)
       json_response({ message: e.message }, :unprocessable_entity)
     end
-  end
+
+    # Resposta JSON com mensagem; Código de status 401 - não autorizado
+    def unauthorized_request(e)
+      json_response({ message: e.message }, :unauthorized)
+    end
+
 end
