a878a78eae1343d70d18080724166a825e0827d6
Remyson Rodrigues <remysonrodrigues@gmail.com>
2020-08-25 15:22:06 -0300

    add refactor routes version

--- tasks-api/config/routes.rb
+++ tasks-api/config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :tasks do
-    resources :items
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # namespace dos controladores sem afetar o URI
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :tasks do
+      resources :items
+    end
   end
+
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
+
 end
