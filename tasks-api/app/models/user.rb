f2807f1be712644cfc4e6de100525148802a5dea
Remyson Rodrigues <remysonrodrigues@gmail.com>
2020-08-25 09:41:33 -0300

    add code model User

--- tasks-api/app/models/user.rb
+++ tasks-api/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  # criptografar senha
+  has_secure_password
+
+  # Associações de modelo
+  has_many :tasks, foreign_key: :created_by
+  # Validações
+  validates_presence_of :name, :email, :password_digest
 end
--- tasks-api/spec/models/user_spec.rb
+++ tasks-api/spec/models/user_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   # Teste de associação
   # garantir que o modelo de usuário tenha uma relação de 1:m com o modelo Task
-  it { should have_many(:todos) }
+  it { should have_many(:tasks) }
   # Testes de validação
   # certifique-se de que nome, e-mail e senha_digest estejam presentes antes de salvar
   it { should validate_presence_of(:name) }
