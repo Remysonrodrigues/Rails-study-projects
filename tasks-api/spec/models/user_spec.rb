1b89a10b66fa5a2ba96ec6a5cbdd2df845e633fe
Remyson Rodrigues <remysonrodrigues@gmail.com>
2020-08-25 09:35:39 -0300

    add user_spec

--- tasks-api/spec/models/user_spec.rb
+++ tasks-api/spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
+# Conjunto de testes para o modelo de usuário
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Teste de associação
+  # garantir que o modelo de usuário tenha uma relação de 1:m com o modelo Task
+  it { should have_many(:todos) }
+  # Testes de validação
+  # certifique-se de que nome, e-mail e senha_digest estejam presentes antes de salvar
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
 end
