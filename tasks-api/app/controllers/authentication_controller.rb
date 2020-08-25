982d7c4e0bbb8f2f0bf5b6b09609f608b238fc24
Remyson Rodrigues <remysonrodrigues@gmail.com>
2020-08-25 11:00:33 -0300

    add authentication_controller

--- tasks-api/app/controllers/authentication_controller.rb
+++ tasks-api/app/controllers/authentication_controller.rb
@@ -1,2 +1,14 @@
 class AuthenticationController < ApplicationController
+  # retornar token de autenticação assim que o usuário for autenticado
+  def authenticate
+    auth_token = AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+    json_response(auth_token: auth_token)
+  end
+
+  private
+
+    def auth_params
+      params.permit(:email, :password)
+    end
+    
 end
